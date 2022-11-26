39ed3c02efcc50cce2d3201a5d22ff99253baf7a
aparstechteam <79406959+aparstechteam@users.noreply.github.com>
2022-11-26 10:56:54 +0600

    marking policy updated

--- assets/lodash.7c7d977f.js
+++ assets/lodash.7c7d977f.js
@@ -1,4 +1,4 @@
-import{O as jt}from"./index.22439ba0.js";var Ri={exports:{}};/**
+import{O as jt}from"./index.617c54d8.js";var Ri={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
